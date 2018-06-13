311f5ca14dc1c8db12aa1a584b2cbdd897f75e3b
Allan Eising <allan.eising@telia.no>
2018-06-13 14:59:06 +0200

    Started work on REST api

--- controllers/rest.rb
+++ controllers/rest.rb
@@ -1,63 +1,42 @@
 class Icing < Sinatra::Base
 
-    # @!group REST API
+  # @!group REST API
 
-    # @method api_tests
-    # Returns all tests, default formatted in JSON
-    get '/api/tests' do
-        data = Tests.select(:id, :crid, :customer, :location, :timestamp, :deadline, :rfs, :deleted).all
-        format_response(data, request.accept)
+  get '/api/v1/config' do
+    content_type 'text/json'
+    reference = nil
+    if params.has_key? :reference
+      # We filter by reference
+      reference = params[:reference]
     end
 
-    # @method api_test_id
-    # returns a given test in JSON
-    get '/api/test/:id' do
-        test = Tests.where(:id => params[:id])
-        if test.count == 0
-            halt(404)
-        else
-            data = test.first.to_hash
-            data[:template_fields] = JSON.parse(data[:template_fields])
-            format_response(data, request.accept)
-        end
+    if reference
+      # Filter on reference
+      orders = Orders.where(:reference => reference).all
+    else
+      orders = Orders.all
     end
 
-    # @method api_test_crid
-    # filters tests by CRID
-    get '/api/test/crid/:crid' do
-        # Method to get test by CRID
-        args = { :crid => params[:crid] }
-        if params.has_key? "rfs"
-            if params[:rfs] =~ /^(1|true)$/
-                args[:rfs] = true
-            elsif params[:rfs] =~ /^(0|false)$/
-                args[:rfs] = nil
-            end
-        end
-        if params.has_key? "deleted"
-            if params[:deleted] =~ /^(1|true)$/
-                args[:deleted] = true
-            elsif params[:deleted] =~ /^(0|false)$/
-                args[:deleted] = nil
-            end
-
-        end
-
-        test = Tests.where(args)
-        if test.count == 0
-            halt(404)
-        else
-            response = []
-            data = test.all
-            data.each do |resp|
-                tresp = resp.to_hash
-                tresp[:template_fields] = JSON.parse(tresp[:template_fields])
-                response << tresp
-            end
-            format_response(response, request.accept)
-        end
+    data = []
+    orders.each do |order|
+      order.order_config.each do |config|
+        data << { "reference" => order.reference, "service_up" => config.up_config, "service_down" => config.down_config }
+      end
     end
 
+    data.to_json
+  end
 
+  get '/api/v1/service' do
+    # This fetches a list of services and their fields
+    content_type 'text/json'
+
+    data = []
+    Forms.exclude(:deleted => true).each do |form|
+      data << { "name" => form.name, "description" => form.description, "fields" => form.defaults }
+    end
+
+    data.to_json
+    end
 
 end
