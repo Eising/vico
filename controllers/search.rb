d05577b02afc70736678c869a9440698a92af16e
Allan Eising <allan.eising@telia.no>
2018-06-22 13:44:54 +0200

    Implemented searching

---
+++ controllers/search.rb
@@ -0,0 +1,16 @@
+class Icing < Sinatra::Base
+  # This is a class that handles searching
+
+  get '/search' do
+    @pagetitle = "Search Results"
+    query = params[:query]
+
+    @template_results = Templates.exclude(:deleted => true).filter(:name => /#{query}/).all
+    @form_results = Forms.exclude(:deleted => true).filter(:name => /#{query}/).all
+    @order_results = Orders.exclude(:deleted => true).filter(:reference => /#{query}/).all
+
+    @results = @template_results.count + @form_results.count + @order_results.count
+
+    erb :'search/results'
+  end
+end
