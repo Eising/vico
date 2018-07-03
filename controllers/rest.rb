96cb3bc99e14760005b62bef3ed6052ded3b6d69
Allan Eising <allan.eising@telia.no>
2018-07-03 15:54:55 +0200

    Converted the REST service to use namespace

--- controllers/rest.rb
+++ controllers/rest.rb
@@ -2,41 +2,48 @@ class Icing < Sinatra::Base
 
   # @!group REST API
 
-  get '/api/v1/config' do
-    content_type 'text/json'
-    reference = nil
-    if params.has_key? :reference
-      # We filter by reference
-      reference = params[:reference]
-    end
+  namespace '/api/v1' do
 
-    if reference
-      # Filter on reference
-      orders = Orders.where(:reference => reference).all
-    else
-      orders = Orders.all
-    end
-
-    data = []
-    orders.each do |order|
-      order.order_config.each do |config|
-        data << { "reference" => order.reference, "service_up" => config.up_config, "service_down" => config.down_config }
+    get '/config' do
+      content_type 'text/json'
+      reference = nil
+      if params.has_key? :reference
+        # We filter by reference
+        reference = params[:reference]
       end
+
+      if reference
+        # Filter on reference
+        orders = Orders.where(:reference => reference).all
+      else
+        orders = Orders.all
+      end
+
+      data = []
+      orders.each do |order|
+        order.order_config.each do |config|
+          data << { "reference" => order.reference, "service_up" => config.up_config, "service_down" => config.down_config }
+        end
+      end
+
+      data.to_json
     end
 
-    data.to_json
+    get '/service' do
+      # This fetches a list of services and their fields
+      content_type 'text/json'
+
+      data = []
+      Forms.exclude(:deleted => true).each do |form|
+        data << { "name" => form.name, "description" => form.description, "fields" => form.defaults }
+      end
+
+      data.to_json
+      end
+
+
+
   end
 
-  get '/api/v1/service' do
-    # This fetches a list of services and their fields
-    content_type 'text/json'
-
-    data = []
-    Forms.exclude(:deleted => true).each do |form|
-      data << { "name" => form.name, "description" => form.description, "fields" => form.defaults }
-    end
-
-    data.to_json
-    end
 
 end
